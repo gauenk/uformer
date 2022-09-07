f92553654ce6d265bc7743d0140c65f237fb63f3
gauenk <kent.gauen@gmail.com>
2022-09-07 14:09:32 -0400

    parse

--- lib/uformer/augmented/parse.py
+++ lib/uformer/augmented/parse.py
@@ -2,6 +2,25 @@
 from ..utils.model_keys import translate_values
 from collections import OrderedDict
 
+def expand_embed_dim(embed_dim):
+    raise NotImplementedError("")
+    # -- default --
+    if isinstance(embed_dim,int):
+        return [[embed_dim,embed_dim] for _ in range(8)]
+
+    # -- init --
+    embed_dims = []
+    _embed_dims = embed_dim.split("_")
+    assert len(_embed_dims) == 6
+
+    # -- pairs of in/out --
+    for l in range(L):
+        dim_l = _embed_dims[l].split("+")
+        print(dim_l)
+        dim_l0,dim_l1 = int(dim_l[0]),int(dim_l[1])
+        embed_dims.append([dim_l0,dim_l1])
+    return embed_dims
+
 def fields2blocks(attn_mode,k,ps,pt,ws,wt,dil,stride0,stride1,
                   nbwd,rbwd,exact,bs,freeze,nblocks=5):
     # return attn_mode,k,ps,pt,ws,wt,dil,stride0,stride1,nbwd,exact
